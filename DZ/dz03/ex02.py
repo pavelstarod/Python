94dba1457e8de62f868e358bc3e1ea254f4bd7b4
Pavel Starodubov <pablostar68@yahoo.com>
2023-01-11 20:39:39 +0300

    add ex02 fix

--- DZ/dz03/ex02.py
+++ DZ/dz03/ex02.py
@@ -12,10 +12,10 @@ for item in range(len(my_list)):
     my_list2.append(int(my_list[item]))
 print(my_list2)
 
-for item in range(len(my_list2)//2):
+for item in range(len(my_list2)):
     if item < len(my_list2)//2:
         my_list3.append(my_list2[item]*my_list2[item*(-1)-1])
-    elif item == (len(my_list2)//2)+1:
+    elif len(my_list2) % 2 == 1 and item == (len(my_list2)//2):
         my_list3.append(my_list2[item]**2)
 
 print(my_list3)
